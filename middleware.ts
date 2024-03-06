a8bcaa9e8db9e3ddaa3cbe81fd5e54e304ef6513
Eddy Gonzalez <gonzalezeddyan@gmail.com>
2024-03-06 13:37:33 -0400

    webhook modfified

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ['/', '/profile'],
-  ignoredRoutes: ['/api/webhooks/clerk']
+  publicRoutes: ['/', '/api/webhooks/clerk'],
   // Routes that can always be accessed, and have
   // no authentication information
 });
